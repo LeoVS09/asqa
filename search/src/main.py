d3c42569ef1072400c40eb933c417e0ffa3a64dc
LeoVS09 <LeoVS09@mail.ru>
2021-08-14 22:54:05 +0300

    add logs when server starting

--- search/src/main.py
+++ search/src/main.py
@@ -10,5 +10,7 @@ logging.info(f'App version: {settings["APP_VERSION"]}')
 
 app = FastAPI(port=settings['SERVER_PORT'])
 
+logging.info(f'Run server at port {settings["SERVER_PORT"]}')
+
 app.include_router(router)
 app.include_router(graphql_router)
