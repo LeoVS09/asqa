5f3d6e7d3f7b3f1ee26e29c1b42c4b9f5b4be04f
leovs09 <leovs010@gmail.com>
2021-08-24 23:48:46 +0300

    remove reindex from search engine

--- search/src/engine/init_engine.py
+++ search/src/engine/init_engine.py
@@ -8,9 +8,6 @@ from .read_models import read_models
 from .IndexSearch import IndexSearch
 from src.db.PassagesDatabaseMongoAdapter import PassagesDatabaseMongoAdapter
 
-REINDEX_SIZE = settings['REINDEX_SIZE']
-MEANINGULL_SIZE = settings['MEANINGULL_SIZE']
-
 def load_and_init_engine():
     passages = PassagesDatabaseMongoAdapter(mongodb_url = settings['MONGODB_URL'])
     index = IndexSearch()
@@ -20,6 +17,4 @@ def load_and_init_engine():
         model = EmbedingModel(model, tokenizer), 
         passages = passages, 
         index = index,
-        reindex_size = REINDEX_SIZE,
-        meaningfull_size = MEANINGULL_SIZE
     )
