8dc54caaabebbc68400b465bd2feefc593489495
leovs09 <leovs010@gmail.com>
2021-09-22 22:58:58 +0300

    test question asking from telegram service

--- integrations/telegram/test/kafka.e2e-spec.ts
+++ integrations/telegram/test/kafka.e2e-spec.ts
@@ -1,6 +1,9 @@
+import { Message } from 'src/retranslator';
 import { KafkaAdapter } from './../src/services';
 
 const timeout = time => new Promise(resolve => setTimeout(resolve, time))
+
+jest.setTimeout(30000);
 
 describe('Kafka (e2e)', () => {
   let kafka: KafkaAdapter;
@@ -10,13 +13,20 @@ describe('Kafka (e2e)', () => {
     await kafka.connect()
   });
 
-  it('push message', async () => {
+  it('Ask question', async () => {
+    const answerPromise = new Promise(resolve => kafka.on(message => {
+      resolve(message);
+    }))
+
     kafka.send({
       meta: { identity: 'test'},
-      text: 'This is a test message from Telegram service'
+      text: 'What software tests made for?'
     })
 
-    await timeout(1000)
+    const answer = await answerPromise as Message
+    console.log('Answer from Kafka:', answer)
 
+    expect(answer.meta.identity).toBe('test')
+    expect(answer.text).toBeDefined();
   });
 });
